c638b962f04baedd2daf6ae2dfb7406890bdca83
Elton Luiz Scholze <elscholze@gmail.com>
2022-10-08 10:18:32 -0300

    fix: fix App.js, Routes/AllRoutes.js, Home/index.jsx, pages/Login/index.jsx

--- react-entrega-kenzie-hub-elton-luiz-scholze/src/App.js
+++ react-entrega-kenzie-hub-elton-luiz-scholze/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
 import { AllRoutes } from "./Routes/AllRoutes";
 
 function App() {
+  const [user, setUser] = useState({});
   return (
     <div className="App">
-      <AllRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        progress={undefined}
+        theme="colored"
+      />
+      <AllRoutes user={user} setUser={setUser} />
     </div>
   );
 }
